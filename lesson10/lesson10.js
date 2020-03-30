4371c8662ee5f4cddceb51831382dbf030a44e52
XiangZhifa <974688383@qq.com>
2020-03-30 18:01:01 +0800

    ts综合应用，封装Mysql操作类

---
+++ lesson10/lesson10.js
@@ -0,0 +1,11 @@
+//用于储存用户名，密码
+var User = /** @class */ (function () {
+    function User() {
+    }
+    return User;
+}());
+var MysqlDb = /** @class */ (function () {
+    function MysqlDb() {
+    }
+    return MysqlDb;
+}());
---
+++ lesson10/lesson10.ts
@@ -0,0 +1,9 @@
+//用于储存用户名，密码
+class User {
+    username: string | undefined;
+    password: string | undefined;
+}
+
+class MysqlDb {
+
+}
